90ea0404fbdf8c0c59cb532b545c9ba9f0754dda
Apswak <7396157+apswak@users.noreply.github.com>
2022-10-18 00:12:24 +0100

    chore: utilise .env for api keys

--- src/helpers/get-api-key/get-api-key.ts
+++ src/helpers/get-api-key/get-api-key.ts
@@ -1,16 +1,18 @@
 import { Network } from "../../types";
 
 export function getApiKey(network: Network) {
+  const env = import.meta.env;
+
   switch (network) {
     case "arbitrum":
-      return "MOCK_TOKEN";
-    case "optimism":
-      return "MOCK_TOKEN";
+      return env.VITE_ARBITRUM_API_KEY;
     case "ethereum":
-      return "MOCK_TOKEN";
+      return env.VITE_ETHEREUM_API_KEY;
+    case "optimism":
+      return env.VITE_OPTIMISM_API_KEY;
     case "polygon":
-      return "MOCK_TOKEN";
+      return env.VITE_POLYGON_API_KEY;
     default:
-      return "MOCK_TOKEN";
+      return "";
   }
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ARBITRUM_API_KEY: string;
+  readonly VITE_ETHEREUM_API_KEY: string;
+  readonly VITE_OPTIMISM_API_KEY: string;
+  readonly VITE_POLYGON_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
