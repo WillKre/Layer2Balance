c0184d78beb470cb5267a8fee9f2358c63fb5492
Apswak <7396157+apswak@users.noreply.github.com>
2022-10-24 00:00:07 +0100

    fix: ts Data type

--- src/helpers/formatValidBalance/formatValidBalance.ts
+++ src/helpers/formatValidBalance/formatValidBalance.ts
@@ -1,5 +1,5 @@
-import { APIResponse } from "../../types";
+import { Data } from "../../types";
 
-export function formatValidBalance(data: APIResponse) {
-  return data.message === "OK" ? data.result : "0";
+export function formatValidBalance(data: Data | undefined) {
+  return data?.message === "OK" ? data.result : "0";
 }
